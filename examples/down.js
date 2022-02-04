941bd5899646479d75c0dbd848ebdde07ac27dfe
Luis Rueda <lurueda@cisco.com>
2022-02-04 11:31:47 -0500

    Add option to specify example
    Co-Authored-By: Jairo Leon <33960025+jaileon@users.noreply.github.com>

--- examples/down.js
+++ examples/down.js
@@ -2,11 +2,20 @@ var Dockerode = require('dockerode');
 var DockerodeCompose = require('../compose');
 
 var docker = new Dockerode();
-var compose = new DockerodeCompose(
-  docker,
-  './test/assets/wordpress_original.yml',
-  'wordpress'
-);
+
+var yamlFile = './test/assets/wordpress_original.yml'
+var projectName = 'wordpress'
+
+if (process.argv.length > 2) {
+  if (process.argv[2] !== undefined) {
+    yamlFile = process.argv[2]
+  }
+  if (process.argv[3] !== undefined) {
+    projectName = process.argv[3]
+  }
+}
+
+var compose = new DockerodeCompose(docker, yamlFile, projectName);
 
 (async () => {
   var state = await compose.down({ volumes: true });
--- examples/pull.js
+++ examples/pull.js
@@ -2,7 +2,20 @@ var Dockerode = require('dockerode');
 var DockerodeCompose = require('../compose');
 
 var docker = new Dockerode();
-var compose = new DockerodeCompose(docker, './test/assets/wordpress.yml', 'wordpress');
+
+var yamlFile = './test/assets/wordpress_original.yml'
+var projectName = 'wordpress'
+
+if (process.argv.length > 2) {
+  if (process.argv[2] !== undefined) {
+    yamlFile = process.argv[2]
+  }
+  if (process.argv[3] !== undefined) {
+    projectName = process.argv[3]
+  }
+}
+
+var compose = new DockerodeCompose(docker, yamlFile, projectName);
 
 (async () => {
   console.log(await compose.pull());
